5353d4df21d1363ac00ca71fe95087a26e9ba913
Ozk18532 <119715835+Ozk18532@users.noreply.github.com>
2025-04-25 17:11:25 -0700

    Add files via upload

---
+++ TJmigranteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TJmigranteApp.swift
+//  TJmigrante
+//
+//  Created by Oz bibriesca on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct TJmigranteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
